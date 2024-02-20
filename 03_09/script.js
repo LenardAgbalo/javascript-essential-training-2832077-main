2f54affcf7e3c909668030eb5d9e1d2b47db4dec
John Lenard Serrano Agbalo <johnlenardagbalo@gmail.com>
2024-02-21 00:04:29 +0800

    first commit

---
+++ 03_09/script.js
@@ -0,0 +1,27 @@
+/**
+ * Create a Backpack object.
+ */
+
+const car = {
+  name: "Everyday Backpack",
+  volume: 30,
+  color: "grey",
+  pocketNum: 15,
+  strapLength: {
+    left: 26,
+    right: 26,
+  },
+  lidOpen: false,
+  toggleLid: function (lidStatus) {
+    this.lidOpen = lidStatus;
+  },
+  newStrapLength: function (lengthLeft, lengthRight) {
+    this.strapLength.left = lengthLeft;
+    this.strapLength.right = lengthRight;
+  },
+  newColorChange: function (colorChange) {
+    this.color = colorChange;
+  },
+};
+
+console.log("The backpack object:", car);
